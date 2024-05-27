cabe3b52a926c74c7de71ff65aa797dc84893230
Willian Ribeiro <willianribeiro709@gmail.com>
2024-05-26 22:18:59 -0300

    first commit

---
+++ [M2S02]-Ex6/index.js
@@ -0,0 +1,16 @@
+function transformarArray(array, transformacao) {
+    return array.map(transformacao);
+}
+
+function dobrar(numero) {
+    return numero * 2;
+}
+
+let numeros = [1, 2, 3, 4, 5];
+console.log(transformarArray(numeros, dobrar)); 
+
+function adicionarCinco(numero) {
+    return numero + 5;
+}
+
+console.log(transformarArray(numeros, adicionarCinco)); 
